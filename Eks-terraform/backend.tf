4bf0961f4b251298eca3d9d0be5c5050319cf741
Bhaskinator <bhaskinator@gmail.com>
2025-03-20 10:39:21 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "reddit-clone123" # Replace with your actual S3 bucket name
+    bucket = "bhaskinator" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
